b9d7f488a235904be0e6dd64ffa79aa7982cedd7
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-05-23 07:06:32 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
 
-interface Point{
-  x: number,
-  y:number
+class Point {
+   constructor(private x?: number, private y?: number){
+  }
+  
+  draw() {
+    console.log("X: " + this.x + ", y: " + this.y);
+  }
 }
 
-let drawPoint = (point: Point) => {
-  // ...
-}
-
-drawPoint({ 
-  x: 1,
-  y: 2
-})
+let point = new Point(1, 2);
+point.draw();
